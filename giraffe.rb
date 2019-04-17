30041798d0bf9ceff65d20df9bd248a990807263
Idan Madar <t882200@gmail.com>
2019-04-17 23:00:22 +0000

    bkp

---
+++ giraffe.rb
@@ -0,0 +1,20 @@
+#~01 print text to console
+# print "Hello World"
+
+#~02 draw a shape
+# puts "Idan"
+# puts "is cool"
+# print "Giraffe Academy\n"
+# puts "   /|"
+# puts "  / |"
+# puts " /  |"
+# puts "/___|"
+
+#~03 variables
+character_name = "John"
+character_age = "35"
+puts ("" + character_name)
+puts ("" + character_age)
+puts ("" + character_name)
+puts ("" + character_age)
+
